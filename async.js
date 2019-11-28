3949c01c90f19cce51f3a97622dbfbe620892a0b
Sergey Gogolev <gogoleff@yandex-team.ru>
2019-11-28 17:12:12 +0500

    Задача «Мальчишник у Аркадия»

---
+++ async.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Сделано задание на звездочку.
+ * Реализована остановка промиса по таймауту.
+ */
+const isStar = true;
+
+/**
+ * Функция паралелльно запускает указанное число промисов
+ *
+ * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
+ * @param {Number} parallelNum - число одновременно исполняющихся промисов
+ * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise<Array>}
+ */
+function runParallel(jobs, parallelNum, timeout = 1000) {
+    // Самая сильная асинхронная магия
+}
+
+module.exports = {
+    runParallel,
+    isStar
+};
