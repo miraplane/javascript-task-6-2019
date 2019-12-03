1bfd00807e45ca20d8ddb7bbcbe9cb0f40ec902e
miraplane <irinamochalova@gmail.com>
2019-12-03 12:33:10 +0500

    Добавлено решение без звездочки

--- async.js
+++ async.js
@@ -4,18 +4,42 @@
  * Сделано задание на звездочку.
  * Реализована остановка промиса по таймауту.
  */
-const isStar = true;
+const isStar = false;
+
+function splitOnSections(array, size) {
+    let subArrays = [];
+    let lengthSubSet = Math.ceil(array.length / size);
+    for (let i = 0; i < lengthSubSet; i++) {
+        let startIndex = i * size;
+        subArrays.push(array.slice(startIndex, startIndex + size));
+    }
+
+    return subArrays;
+}
 
 /**
  * Функция паралелльно запускает указанное число промисов
  *
- * @param {Function<Promise>[]} jobs – функции, которые возвращают промисы
+ * @param {Function<Promise>[]} jobs – функции, которые возвращают промис
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
- * @param {Number} timeout - таймаут работы промиса
  * @returns {Promise<Array>}
  */
-function runParallel(jobs, parallelNum, timeout = 1000) {
-    // Самая сильная асинхронная магия
+function runParallel(jobs, parallelNum) {
+    let promiseSet = splitOnSections(jobs, parallelNum);
+
+    return (async function () {
+        let result = [];
+        for (let i = 0; i < promiseSet.length; i++) {
+            try {
+                let pr = await Promise.all(promiseSet[i].map(promise => promise()));
+                result = result.concat(pr);
+            } catch (e) {
+                result.push(new Error(e));
+            }
+        }
+
+        return result;
+    }());
 }
 
 module.exports = {
